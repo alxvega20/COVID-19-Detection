81b51fca16917bcfeda5b645598ff3b613646d3f
Andy Tran <andytran557@gmail.com>
2025-04-30 22:57:24 -0500

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,1 +1,10 @@
+import torch.nn as nn
+from torchvision import models
+
+class CovidResNet(nn.Module):
+    def __init__(self, num_classes: int = 2):
+        super().__init__()
+        self.backbone = models.resnet18(weights="DEFAULT")
+        self.backbone.fc = nn.Linear(self.backbone.fc.in_features, num_classes)
+    def forward(self, x): return self.backbone(x)
 
